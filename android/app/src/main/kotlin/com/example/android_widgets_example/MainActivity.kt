3b2c0d659f8bf939424b75685f7a12900daf1be4
greymag <greymag@gmail.com>
2020-06-04 17:37:16 +0300

    Add widget by click (if supported).

--- android/app/src/main/kotlin/com/example/android_widgets_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/android_widgets_example/MainActivity.kt
@@ -1,6 +1,40 @@
 package com.example.android_widgets_example
 
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.os.Build
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "example.flutter.innim.ru/android_widgets"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+
+            if (call.method == "addWidget") {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    addWidget()
+                    result.success(true)
+                } else {
+                    result.error("UNAVAILABLE",
+                            "Pin request is not available. Add widget from Android menu",
+                            null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun addWidget() {
+        val appWidgetManager: AppWidgetManager = context.getSystemService(AppWidgetManager::class.java)
+        val myProvider = ComponentName(context, FlutterExampleAppWidget::class.java)
+        appWidgetManager.requestPinAppWidget(myProvider, null, null)
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,6 +29,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const platform =
+      MethodChannel('example.flutter.innim.ru/android_widgets');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +39,22 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text('Example'),
+        child: Builder(
+          builder: (context) => RaisedButton(
+            child: Text('Add widget'),
+            onPressed: () async {
+              String msg;
+              try {
+                final bool result = await platform.invokeMethod('addWidget');
+                msg = result ? 'Widget added' : "Can't add widget";
+              } on PlatformException catch (e) {
+                msg = "Failed to add widget: '${e.message}'.";
+              }
+
+              Scaffold.of(context).showSnackBar(SnackBar(content: Text(msg)));
+            },
+          ),
+        ),
       ),
     );
   }
